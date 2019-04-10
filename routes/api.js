2e38f90728d6070ca2a93e2ab20b139a3fed51bb
Jennie  Chen <jenniechen@levy-ve501-0045.apn.wlan.upenn.edu>
2019-04-09 20:06:47 -0400

    removed error in api.js

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var Question = require('../models/question.js')
 
 router.get('/getQuestions', function (req, res, next) {
-  questions = Question.find({}, function (err, result) {
 })
 
 router.post('/addQuestion', function (req, res, next) {
